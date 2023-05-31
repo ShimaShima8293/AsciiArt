91a1693698cf05cb96fe6736b454a489110fbf14
Shima Shima <shimashima8293@gmail.com>
2023-05-31 19:14:13 +0900

    Updated `main.py` - Added `exit_` - Added `Image.open()` to `generate` - Added `pathVar` - Added `exitButton`

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from tkinter import ttk
 from tkinter import filedialog
 from PIL import Image
 
-imagepath = ""
 PADX = 10
 PADY = 10
 
@@ -15,14 +14,20 @@ def askPath():
     pathEntry.insert(0, tmp.name)
 
 def generate():
-    print(imagepath)
+    print("Generating for: " + pathVar.get())
+    Image.open(pathVar.get())
+
+def exit_():
+    root.destroy()
+    exit(0)
 
 root = tk.Tk()
 root.geometry("600x400")
 
 rootFrame = ttk.Frame(root)
 
-pathEntry = ttk.Entry(rootFrame)
+pathVar = tk.StringVar()
+pathEntry = ttk.Entry(rootFrame, textvariable=pathVar)
 pathEntry.pack(anchor=tk.W, expand=True, side=tk.LEFT, padx=PADX, pady=PADY)
 
 browseButton = ttk.Button(rootFrame, text="Browse...", command=askPath)
@@ -30,6 +35,9 @@ browseButton.pack(anchor=tk.E, side=tk.LEFT, padx=PADX, pady=PADY)
 
 generateButton = ttk.Button(rootFrame, text="Generate", command=generate)
 generateButton.pack(side=tk.TOP, padx=PADX, pady=PADY)
+
+exitButton = ttk.Button(rootFrame, text="Exit", command=exit_)
+exitButton.pack(side=tk.TOP, padx=PADX, pady=PADY)
 
 rootFrame.pack()
 
